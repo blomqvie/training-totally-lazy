5d81392ca9706d6467aadaecb4b5e16cc800a632
Eetu Blomqvist <eetu.blomqvist@reaktor.fi>
2015-03-18 11:07:44 +0900

    Add async json loading with RxJava

--- app/src/main/java/fi/reaktor/training/totallylazy/data/Beers.java
+++ app/src/main/java/fi/reaktor/training/totallylazy/data/Beers.java
@@ -7,12 +7,30 @@ import com.googlecode.totallylazy.Sequence;
 import java.io.IOException;
 import java.io.InputStream;
 
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
 public class Beers {
 
     private static Sequence<Beer> beers;
-    // TODO: static init is nasty and JSON should be read asynchronously e.g. with RxJava
+    private static Observable<Sequence<Beer>> beerObservable;
+
     public static Beers init(InputStream inputStream, ObjectMapper objectMapper) throws IOException {
         beers = objectMapper.readValue(inputStream, new TypeReference<Sequence<Beer>>() {});
+
+        // TODO: not used yet
+        beerObservable = Observable.<Sequence<Beer>>create((subscriber) -> {
+            try {
+                Sequence<Beer> beerSeq = objectMapper.readValue(inputStream, new TypeReference<Sequence<Beer>>() {
+                });
+                subscriber.onNext(beerSeq);
+                subscriber.onCompleted();
+            } catch (IOException e) {
+                subscriber.onError(e);
+            }
+        }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+
         return new Beers();
     }
 
